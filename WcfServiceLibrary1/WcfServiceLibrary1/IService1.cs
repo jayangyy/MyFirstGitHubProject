b67916453007975d16355c61f489cdfea81606ca
jayangyy <361300115@qq.com>
2015-08-06 15:25:55 +0800

    AddFloder

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/IService1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService1”。
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "AZwcfXML", ResponseFormat = WebMessageFormat.Xml, RequestFormat = WebMessageFormat.Xml,BodyStyle=WebMessageBodyStyle.Bare)]
+        List<User> AZwcfXML();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "AZwcfJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json,BodyStyle = WebMessageBodyStyle.Bare)]
+        List<User> AZwcfJson();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "AZwcfStr/{Str},{Str1}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        List<Vide> AZwcfStr(string Str,string Str1);
+
+        // TODO: 在此添加您的服务操作
+    }
+
+    // 使用下面示例中说明的数据协定将复合类型添加到服务操作
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Age { get; set; }
+        [DataMember]
+        public string Addr { get; set; }
+    }
+
+    [DataContract]
+    public class Vide
+    {
+        [DataMember]
+        public string FileName { get; set; }
+        [DataMember]
+        public string JumpTime { get; set; }
+    }  
+}
---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Activation;
+
+namespace WcfServiceLibrary1
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public List<Vide> AZwcfStr(string Str,string Str1)
+        {
+            string stri = Str;
+            string stri1 = Str1;
+            if (stri == "123")
+            {
+                List<Vide> list = new List<Vide>();
+                list.Add(new Vide { FileName = "\\\\10.201.18.232\\e\\2014-11\\2014-11-28\\2014-11-28 044435\\2014-11-28 044435_1.Mp4", JumpTime = "0" });
+                return list;
+            }
+            else
+            {
+                List<Vide> list = new List<Vide>();
+                list.Add(new Vide { FileName = "\\\\10.201.18.232\\e\\2014-11\\2014-11-28\\2014-11-28 044435\\2014-11-28 044435_1.Mp4", JumpTime = "30" });
+                return list;
+            }
+        }
+
+        public List<User> AZwcfXML()
+        {
+            List<User> list = new List<User>();
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "123"});
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "234" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "345" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "456" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "567" });
+            return list;
+        }
+
+        public List<User> AZwcfJson()
+        {
+            List<User> list = new List<User>();
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "567" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "456" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "345" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "234" });
+            list.Add(new User { Name = "Bill Gates", Age = "YouYi East Road", Addr = "123" });
+            return list;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
